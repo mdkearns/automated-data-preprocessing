483be05e7964eb332b36704817fa4c54fb24997a
Matt D. Kearns <mattdkearns@gmail.com>
2018-07-07 00:27:28 -0400

    commit make_clean utility stub

---
+++ make_clean.py
@@ -0,0 +1,12 @@
+# make_clean is a command line utility program that automatically
+# performs common data preprocessing tasks on your uncleaned data sets.
+
+# Usage: python [options] make_clean.py /file/path/to/data/
+
+import os
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
