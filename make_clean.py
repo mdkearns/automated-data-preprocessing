1f628ac780ab4d0d9f1c26f6b45a2d276f84866b
Matt D. Kearns <mattdkearns@gmail.com>
2018-07-09 12:38:19 -0400

    add argument parsing

--- make_clean.py
+++ make_clean.py
@@ -15,9 +15,16 @@ def main():
     parser = argparse.ArgumentParser(description=output.description)
 
     parser.add_argument('filePath', metavar='filePath', type=str, help='Path to uncleaned data file')
-    parser.add_argument('-a', help='All')
+    parser.add_argument('-a', '--all',         action='store_const', const='a', help=output.options['-a'])
+    parser.add_argument('-c', '--categorical', action='store_const', const='c', help=output.options['-c'])
+    parser.add_argument('-f', '--fields',      action='store_const', const='f', help=output.options['-f'])
+    parser.add_argument('-i', '--interpolate', action='store_const', const='i', help=output.options['-i'])
+    parser.add_argument('-o', '--outliers',    action='store_const', const='o', help=output.options['-o'])
+    parser.add_argument('-v', '--version',     action='version', version='v1.0.0')
 
     args = parser.parse_args()
+
+    print(args)
 
     # if sys.argv[-1] == '--help':
     #     print(output.info)
--- output/__init__.py
+++ output/__init__.py
@@ -1,15 +1,9 @@
-info = "make_clean v1.0.0 (July, 2018) by Matthew D. Kearns\n"
+description = "The make_clean command line utility program automatically " \
+              "performs common data preprocessing tasks on your uncleaned data sets.\n"
 
-description = "The make_clean command line utility program " \
-              "automatically performs common data preprocessing " \
-              "tasks on your uncleaned data sets.\n"
-
-
-usage = "Usage\n\tpython make_clean.py [options] /path/to/data/file\n" \
-          "\nOptions\n" \
-          "\t-a\t\tAll\n" \
-          "\t-c\t\tFile contains categorical data\n" \
-          "\t-h\t\tFile contains headers\n" \
-          "\t-i\t\tInterpolate missing values\n" \
-          "\t-o\t\tOutlier detection and removal\n"
+options = {'-a': 'all',
+           '-c': 'file contains categorical data',
+           '-f': 'file contains field names (headers)',
+           '-i': 'interpolate missing values',
+           '-o': 'outlier detection and removal'}
 
