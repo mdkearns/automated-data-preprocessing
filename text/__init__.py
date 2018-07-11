a3409f4879cec582b4c5d394277120da1c703393
Matt D. Kearns <mattdkearns@gmail.com>
2018-07-11 08:38:03 -0400

    add more descriptive text for --help

--- text/__init__.py
+++ text/__init__.py
@@ -1,7 +1,7 @@
 description = "The make_clean command line utility program automatically " \
               "performs common data preprocessing tasks on your uncleaned data sets.\n"
 
-options = {'-a': 'all',
+options = {'-a': '-c, -i, -o',
            '-c': 'file contains categorical data',
            '-i': 'interpolate missing values',
            '-m': 'file is missing field names',
